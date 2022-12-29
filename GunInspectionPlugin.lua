330b30899b4c431e11ddf6ce1c5bfc4f4c14b51e
Preakrust <118760041+Preakrust@users.noreply.github.com>
2022-12-29 09:00:31 +0100

    Create GunInspectionPlugin.lua

---
+++ GunInspectionPlugin.lua
@@ -0,0 +1,13 @@
+game.ReplicatedStorage.Update_Plugins.Event:Connect(function(data)
+    local Inpsecting = false
+    if data.Method == "Gun_Inspecting_Started" then
+        -- Update your gun engine plugins here, using the Gun_ID passed in the data argument
+        print(data.Tool.Internal_Cache.Ammo.Value)
+        Inpsecting = true
+    elseif data.Method == "Gun_Inspecting_Stoped" then
+        
+        print("Gun stoped inspecting")
+        Inpsecting = false
+    
+    end
+end)
