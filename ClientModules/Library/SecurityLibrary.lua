d0a189a27990109433804921c86b661cd457736d
Preakrust <118760041+Preakrust@users.noreply.github.com>
2023-01-22 14:59:25 +0100

    Update SecurityLibrary.lua

--- ClientModules/Library/SecurityLibrary.lua
+++ ClientModules/Library/SecurityLibrary.lua
@@ -1,1 +1,13 @@
+local module = {}
 
+local ID = "gWdfUwYCls7oMF3ov6PBYuI76kSQwGYCyoOsVAV"
+
+--Kinda basic but we only need that for client security library--
+
+function module.Authed (Tool)
+	if not Tool or not Tool:IsA("Tool") then return end
+
+	if Tool:FindFirstChild(ID) then return true else return false end
+end
+
+return module
