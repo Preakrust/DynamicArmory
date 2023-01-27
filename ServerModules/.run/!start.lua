6694b2f680e8a764f2a41dfa35c81fda73c570c8
Preakrust <118760041+Preakrust@users.noreply.github.com>
2023-01-27 19:15:13 +0100

    Update !start.lua

--- ServerModules/.run/!start.lua
+++ ServerModules/.run/!start.lua
@@ -1,1 +1,37 @@
+--!strict
+
+return function ()
+	
+	--Player Loader--
+	
+	local Player = require(script.Parent.Parent[".ressources"]["{GetPlayer}"])
+	local Character = Player.Character or Player.CharacterAppearanceLoaded:Wait()
+	
+	--Open Library--
+	
+	local OpenLibrary = script.Parent.Parent.OpenLibrary
+	local PackageManager = require(OpenLibrary["{PackageManager}"])
+	
+	--Package--
+	local PackageArgs = PackageManager.NewVirtualPackageArgs.New("DAP", "45875852", "Omega Software", "0.0.0.2")
+	local DAP = PackageManager.NewVirtualPackage(PackageArgs)
+	
+	
+	--Add libraries inside the package--
+	
+	
+	for _, v in pairs(script.Parent.Parent[".ressources"][".PackagesRessources"]:GetChildren()) do
+		if v:IsA("ModuleScript") then
+			PackageManager.ImportModuleToPackage(DAP, v, v.Name)
+		end
+	end
+	
+	
+	--Start the code stuff idk--
+	
+	
+	DAP["{ServerManager}"]()
+	
+	
+end
 
